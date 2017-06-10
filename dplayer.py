342f4260bc579393be9a6d4206f72770a9cb2456
meLog <meLog89@gmail.com>
2017-06-10 12:09:02 +0200

    deploy error handling

--- dplayer.py
+++ dplayer.py
@@ -74,26 +74,28 @@ class PokerPlayerAPI(Resource):
 
         #straight
 
-
-        #three of a kind
-        for i in range(0, howManyBoardCards):
-            for j in range(0, howManyBoardCards):
-                for k in range(0, howManyBoardCards):
-                    if allCards[i][0] == allCards[j][0] && allCards[j][0] == allCards[k][0]
-                        return data['min_bid'] * 3
-
-        #one pair and two pairs
-        for i in range(0, howManyBoardCards):
-            for j in range(1, howManyBoardCards):
-                if allCards[i][0] == allCards[j][0]:
-                    allCards.pop([i])
-                    allCards.pop([j])
-                    howManyBoardCards = len(allCards)
+        try:
+            #three of a kind
+            for i in range(0, howManyBoardCards):
+                for j in range(0, howManyBoardCards):
                     for k in range(0, howManyBoardCards):
-                        for l in range(0, howManyBoardCards):
-                            if allCards[k][0] == allCards[l][0]:
-                                return data['min_bid'] * 2
-                    return data['min_bid']
+                        if allCards[i][0] == allCards[j][0] & allCards[j][0] == allCards[k][0]
+                            return data['min_bid'] * 3
+
+            #one pair and two pairs
+            for i in range(0, howManyBoardCards):
+                for j in range(1, howManyBoardCards):
+                    if allCards[i][0] == allCards[j][0]:
+                        allCards.pop([i])
+                        allCards.pop([j])
+                        howManyBoardCards = len(allCards)
+                        for k in range(0, howManyBoardCards):
+                            for l in range(0, howManyBoardCards):
+                                if allCards[k][0] == allCards[l][0]:
+                                    return data['min_bid'] * 2
+                        return data['min_bid']
+        except ValueError:
+            return data['min_bid']
 
 
         return data['min_bid']
